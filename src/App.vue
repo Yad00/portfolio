2c184894484f44ba7bb4d27b0fe901089a260d95
Yad00 <yad-1999@hotmail.com>
2023-06-12 18:23:05 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <Navbar />
+    <div
+      class="hidden md:block md:left-5 md:right-auto md:w-10 md:fixed md:bottom-0 md:z-10 md:text-[#a8b2d1] md:leading-[1.3] lg:left-10 lg:right-auto"
+    >
+      <ul
+        class="flex flex-col items-center m-0 p-0 list-none after:content-[''] after:block after:w-[1px] after:h-[90px] after:my-0 after:mx-auto after:bg-[#a8b2d1]"
+      >
+        <li class="list-item">
+          <a
+            href="https://github.com/Yad00"
+            target="_blank"
+            class="p-[10px] inline-block no-underline text-inherit relative cursor-pointer"
+          >
+            <svg
+              fill="#a8b2d1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 72 72"
+              class="w-8 h-8 transition duration-150 ease-in hover:-translate-y-1 hover:fill-[#94ffed]"
+            >
+              <path
+                d="M36,12c13.255,0,24,10.745,24,24c0,10.656-6.948,19.685-16.559,22.818c0.003-0.009,0.007-0.022,0.007-0.022	s-1.62-0.759-1.586-2.114c0.038-1.491,0-4.971,0-6.248c0-2.193-1.388-3.747-1.388-3.747s10.884,0.122,10.884-11.491	c0-4.481-2.342-6.812-2.342-6.812s1.23-4.784-0.426-6.812c-1.856-0.2-5.18,1.774-6.6,2.697c0,0-2.25-0.922-5.991-0.922	c-3.742,0-5.991,0.922-5.991,0.922c-1.419-0.922-4.744-2.897-6.6-2.697c-1.656,2.029-0.426,6.812-0.426,6.812	s-2.342,2.332-2.342,6.812c0,11.613,10.884,11.491,10.884,11.491s-1.097,1.239-1.336,3.061c-0.76,0.258-1.877,0.576-2.78,0.576	c-2.362,0-4.159-2.296-4.817-3.358c-0.649-1.048-1.98-1.927-3.221-1.927c-0.817,0-1.216,0.409-1.216,0.876s1.146,0.793,1.902,1.659	c1.594,1.826,1.565,5.933,7.245,5.933c0.617,0,1.876-0.152,2.823-0.279c-0.006,1.293-0.007,2.657,0.013,3.454	c0.034,1.355-1.586,2.114-1.586,2.114s0.004,0.013,0.007,0.022C18.948,55.685,12,46.656,12,36C12,22.745,22.745,12,36,12z"
+              />
+              github
+            </svg>
+          </a>
+        </li>
+        <li class="list-item">
+          <a
+            href="https://www.instagram.com/yadaraz/"
+            target="_blank"
+            class="p-[10px] inline-block no-underline text-inherit relative cursor-pointer"
+          >
+            <svg
+              fill="#a8b2d1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+              class="w-8 h-8 transition duration-150 ease-in hover:-translate-y-1 hover:fill-[#94ffed]"
+            >
+              <path
+                d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"
+              />
+              instagram
+            </svg>
+          </a>
+        </li>
+        <li class="list-item">
+          <a
+            href="https://github.com/Yad00"
+            target="_blank"
+            class="p-[10px] inline-block no-underline text-inherit relative cursor-pointer"
+          >
+            <svg
+              fill="#a8b2d1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+              class="w-8 h-8 transition duration-150 ease-in hover:-translate-y-1 hover:fill-[#94ffed]"
+            >
+              <path
+                d="M 9 4 C 6.2504839 4 4 6.2504839 4 9 L 4 41 C 4 43.749516 6.2504839 46 9 46 L 41 46 C 43.749516 46 46 43.749516 46 41 L 46 9 C 46 6.2504839 43.749516 4 41 4 L 9 4 z M 9 6 L 41 6 C 42.668484 6 44 7.3315161 44 9 L 44 41 C 44 42.668484 42.668484 44 41 44 L 9 44 C 7.3315161 44 6 42.668484 6 41 L 6 9 C 6 7.3315161 7.3315161 6 9 6 z M 14 11.011719 C 12.904779 11.011719 11.919219 11.339079 11.189453 11.953125 C 10.459687 12.567171 10.011719 13.484511 10.011719 14.466797 C 10.011719 16.333977 11.631285 17.789609 13.691406 17.933594 A 0.98809878 0.98809878 0 0 0 13.695312 17.935547 A 0.98809878 0.98809878 0 0 0 14 17.988281 C 16.27301 17.988281 17.988281 16.396083 17.988281 14.466797 A 0.98809878 0.98809878 0 0 0 17.986328 14.414062 C 17.884577 12.513831 16.190443 11.011719 14 11.011719 z M 14 12.988281 C 15.392231 12.988281 15.94197 13.610038 16.001953 14.492188 C 15.989803 15.348434 15.460091 16.011719 14 16.011719 C 12.614594 16.011719 11.988281 15.302225 11.988281 14.466797 C 11.988281 14.049083 12.140703 13.734298 12.460938 13.464844 C 12.78117 13.19539 13.295221 12.988281 14 12.988281 z M 11 19 A 1.0001 1.0001 0 0 0 10 20 L 10 39 A 1.0001 1.0001 0 0 0 11 40 L 17 40 A 1.0001 1.0001 0 0 0 18 39 L 18 33.134766 L 18 20 A 1.0001 1.0001 0 0 0 17 19 L 11 19 z M 20 19 A 1.0001 1.0001 0 0 0 19 20 L 19 39 A 1.0001 1.0001 0 0 0 20 40 L 26 40 A 1.0001 1.0001 0 0 0 27 39 L 27 29 C 27 28.170333 27.226394 27.345035 27.625 26.804688 C 28.023606 26.264339 28.526466 25.940057 29.482422 25.957031 C 30.468166 25.973981 30.989999 26.311669 31.384766 26.841797 C 31.779532 27.371924 32 28.166667 32 29 L 32 39 A 1.0001 1.0001 0 0 0 33 40 L 39 40 A 1.0001 1.0001 0 0 0 40 39 L 40 28.261719 C 40 25.300181 39.122788 22.95433 37.619141 21.367188 C 36.115493 19.780044 34.024172 19 31.8125 19 C 29.710483 19 28.110853 19.704889 27 20.423828 L 27 20 A 1.0001 1.0001 0 0 0 26 19 L 20 19 z M 12 21 L 16 21 L 16 33.134766 L 16 38 L 12 38 L 12 21 z M 21 21 L 25 21 L 25 22.560547 A 1.0001 1.0001 0 0 0 26.798828 23.162109 C 26.798828 23.162109 28.369194 21 31.8125 21 C 33.565828 21 35.069366 21.582581 36.167969 22.742188 C 37.266572 23.901794 38 25.688257 38 28.261719 L 38 38 L 34 38 L 34 29 C 34 27.833333 33.720468 26.627107 32.990234 25.646484 C 32.260001 24.665862 31.031834 23.983076 29.517578 23.957031 C 27.995534 23.930001 26.747519 24.626988 26.015625 25.619141 C 25.283731 26.611293 25 27.829667 25 29 L 25 38 L 21 38 L 21 21 z"
+              />
+              linkedin
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <main
+      class="py-0 px-[25px] sm:px-[50px] md:px-[100px] lg:px-[150px] my-0 mx-auto w-full max-w-[1600px] min-h-screen bg-[#0A192F]"
+    >
+      <section
+        class="flex justify-center flex-col items-start min-h-[90.5vh] h-auto sm:h-[90.5vh] my-0 mx-auto pt-[100px] sm:pt-0 pb-[100px] px-0 max-w-5xl"
+      >
+        <div>
+          <h1
+            class="mt-0 mr-0 mb-[30px] ml-1 text-[#94ffed] font-mono font-normal text-[clamp(14px,_5vw,_16px)] leading-[1.1]"
+          >
+            Hello, my name is
+          </h1>
+        </div>
+        <div>
+          <h2
+            class="m-0 text-[clamp(40px,_8vw,_80px)] font-semibold font-sans text-[#ccd6f6] leading-[1.1]"
+          >
+            Yad Araz.
+          </h2>
+        </div>
+        <div>
+          <h3
+            class="m-0 text-[clamp(40px,_8vw,_80px)] mt-[5px] text-[#8892b0] font-semibold font-sans leading-[0.9]"
+          >
+            I bring ideas to life on the web.
+          </h3>
+        </div>
+        <div>
+          <p
+            class="mx-0 mb-0 mt-5 max-w-[540px] text-[#8892b0] font-sans text-xl leading-[1.3]"
+          >
+            As a Full Stack Developer, my expertise lies in crafting
+            extraordinary digital experiences by leveraging my skills in both
+            frontend and backend development.
+          </p>
+        </div>
+        <div>
+          <a
+            href="./Yad-Araz-CV.pdf"
+            target="_blank"
+            class="text-[#94ffed] bg-transparent border border-solid border-[#94ffed] py-5 px-7 text-sm font-mono leading-none no-underline cursor-pointer mt-[50px] inline-block relative hover:bg-opacity-5 hover:bg-[#94ffed] duration-200 ease-in"
+            >Check out my CV</a
+          >
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,104 @@
+<template>
+  <nav
+    class="bg-[#0A192F] grid grid-cols-[0.5fr_5fr_1fr] md:grid-cols-[1fr_5fr_1fr_5fr_1fr] top-0 z-10 h-[100px] w-full sticky"
+  >
+    <h2
+      class="text-2xl block font-bold self-center col-start-2 col-end-3 text-[#94ffed]"
+    >
+      Yad Araz
+    </h2>
+    <ul
+      class="hidden md:flex md:flex-row md:justify-around md:items-center md:col-start-4 md:col-end-5 md:font-bold md:text-[#ccd6f6]"
+    >
+      <li class="list-item">
+        <a
+          href="#"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >Home</a
+        >
+      </li>
+      <li class="list-item">
+        <a
+          href="#portfolio"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >Portfolio</a
+        >
+      </li>
+      <li class="list-item">
+        <a
+          href="#about"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >About me</a
+        >
+      </li>
+      <li class="list-item">
+        <a
+          href="#contact"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >Contact</a
+        >
+      </li>
+    </ul>
+
+    <ul
+      :class="`fixed h-[100vh] w-[60vw] z-11 right-0 top-0 bg-[#112240] text-[#ccd6f6] flex flex-col justify-around items-center shadow-2xl duration-200 ease-out md:hidden ${
+        is_expanded ? 'translate-x-[0vw]' : 'translate-x-[100vw]'
+      }`"
+    >
+      <li class="list-item">
+        <a
+          href="#"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >HOME</a
+        >
+      </li>
+      <li class="list-item">
+        <a
+          href="#portfolio"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >PORTFOLIO</a
+        >
+      </li>
+      <li class="list-item">
+        <a
+          href="#about"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >ABOUT ME</a
+        >
+      </li>
+      <li class="list-item">
+        <a
+          href="#contact"
+          class="text-[0.9rem] cursor-pointer duration-200 ease-in-out font-mono underline decoration-[#94ffed] hover:decoration-2 hover:text-slate-100"
+          >CONTACT</a
+        >
+      </li>
+    </ul>
+
+    <div
+      class="col-start-3 col-end-4 self-center justify-self-center md:hidden"
+    >
+      <button @click="ToggleMenu">
+        <span
+          :class="`material-icons text-[#94ffed] text-4xl duration-200 ease-out ${
+            is_expanded ? '-rotate-180' : ''
+          }`"
+        >
+          {{ is_expanded ? 'close' : 'menu' }}
+        </span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const is_expanded = ref(localStorage.getItem('is_expanded') === 'true');
+
+const ToggleMenu = () => {
+  is_expanded.value = !is_expanded.value;
+
+  localStorage.setItem('is_expanded', is_expanded.value);
+};
+</script>
